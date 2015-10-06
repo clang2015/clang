8c3f41147a135bebada3d295691c947d6d935fd5
weizhengtian <weizhengtian@weizhengtian-PC>
2015-10-06 21:01:37 +0800

    2

---
+++ classes/stringutils.c
@@ -0,0 +1,7 @@
+#include "stringutils.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+int StrCmp(char * s1, char * s2){
+    
+}
---
+++ include/stringutils.h
@@ -0,0 +1,25 @@
+/* 
+ * File:   stringutils.h
+ * Author: weizhengtian
+ *
+ * Created on 2015年10月6日, 下午8:59
+ */
+
+#ifndef STRINGUTILS_H
+#define	STRINGUTILS_H
+
+#ifdef	__cplusplus
+extern "C" {
+#endif
+    
+int StrCmp(char * s1, char * s2);
+
+
+
+
+#ifdef	__cplusplus
+}
+#endif
+
+#endif	/* STRINGUTILS_H */
+
